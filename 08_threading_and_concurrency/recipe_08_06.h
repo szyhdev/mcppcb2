86f2842c755d4d423a00709c5b48143672a5f6fa
rhd <szyuanhua@163.com>
2025-12-23 16:47:03 +0800

    recipe_08_06: review

--- 08_threading_and_concurrency/recipe_08_06.h
+++ 08_threading_and_concurrency/recipe_08_06.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <iostream>
+#include "recipe_08_common.h"
+
 #include <future>
 
 namespace recipe_08_06
@@ -8,46 +9,26 @@ namespace recipe_08_06
 
 using namespace std::chrono_literals;
 
-std::mutex g_mutex;
-
 void produce_value(std::promise<int> &p)
 {
-    std::cout << "produce_value: entering" << std::endl;
-
-    // simulate long running operation
-    {
-        std::this_thread::sleep_for(2s);
-    }
-
+    std::this_thread::sleep_for(2s);
     p.set_value(42);
-
     std::cout << "produce_value: value set" << std::endl;
 }
 
 void consume_value(std::future<int> &f)
 {
-    std::cout << "consume_value: entering" << std::endl;
-
     auto value = f.get();
-
     std::cout << "consume_value: value got" << std::endl;
-
-    std::lock_guard<std::mutex> lock(g_mutex);
     std::cout << value << std::endl;
 }
 
 void produce_exception(std::promise<int> &p)
 {
-    std::cout << "produce_exception: entering" << std::endl;
-
-    // simulate long running operation
-    {
-        std::this_thread::sleep_for(2s);
-    }
-
+    std::this_thread::sleep_for(2s);
     try {
-       std::cout << "produce_exception: exception occurred" << std::endl;
-        throw std::runtime_error("an error has occurred!");
+        std::cout << "produce_exception: exception occurred" << std::endl;
+        throw std::runtime_error("error");
     }
     catch (...) {
         p.set_exception(std::current_exception());
@@ -56,15 +37,12 @@ void produce_exception(std::promise<int> &p)
 
 void consume_exception(std::future<int> &f)
 {
-    std::cout << "consume_exception: entering" << std::endl;
-
-    std::lock_guard<std::mutex> lock(g_mutex);
     try {
         std::cout << f.get() << std::endl;
     }
     catch (std::exception const &e) {
         std::cout << "consume_exception: exception got" << std::endl;
-        std::cout << "exception: " << e.what() << std::endl;
+        std::cout << e.what() << std::endl;
     }
 }
 
@@ -80,6 +58,7 @@ void execute()
 
         t1.join();
         t2.join();
+        std::cout << std::endl;
     }
 
     // communicate an exception from one thread to another
