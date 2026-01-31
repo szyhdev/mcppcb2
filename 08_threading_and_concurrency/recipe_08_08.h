f24f703035b73b8324d3ac354c33dae35b295c12
rhd <szyuanhua@163.com>
2026-01-31 13:49:39 +0800

    recipe_08_08: review

--- 08_threading_and_concurrency/recipe_08_08.h
+++ 08_threading_and_concurrency/recipe_08_08.h
@@ -11,26 +11,6 @@ namespace recipe_08_08
 
 using namespace std::chrono_literals;
 
-#ifdef HAS_ATOMIC_REF
-void do_count(int &c)
-{
-    std::atomic_ref<int> counter { c };
-
-    std::vector<std::thread> threads;
-    for (int i = 0; i < 10; ++i) {
-        threads.emplace_back([&counter] () {
-            for (int i = 0; i < 10; ++i) {
-                ++counter;
-            }
-        });
-    }
-
-    for (auto &t : threads) {
-        t.join();
-    }
-}
-#endif
-
 std::atomic<int> int_value { 0 };
 std::mutex io_mutex;
 
@@ -65,7 +45,7 @@ void summation(int id)
     while (!ready.load()) {
     }
 
-    long total = 0;
+    unsigned long total = 0;
     for (int i = 0; i < 100000000; ++i) {
         total += i;
     }
@@ -75,18 +55,18 @@ void summation(int id)
     }
 }
 
-std::vector<int> generate_random()
+std::vector<unsigned int> generate_random()
 {
     std::random_device rd;
     auto generator = std::mt19937 { rd() };
     auto dist = std::uniform_int_distribution<> { 1, 99 };
-    std::vector<int> numbers(10000000, 0);
+    std::vector<unsigned int> numbers(100000000, 0);
     std::generate(std::begin(numbers), std::end(numbers),
             [&dist, &generator] {
                 return dist(generator);
             });
 
-    return numbers;
+    return std::move(numbers);
 }
 
 template <typename T,
@@ -110,6 +90,26 @@ public:
     }
 };
 
+#ifdef HAS_ATOMIC_REF
+void do_count(int &c)
+{
+    std::atomic_ref<int> counter { c };
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 10; ++i) {
+        threads.emplace_back([&counter] () {
+            for (int i = 0; i < 10; ++i) {
+                ++counter;
+            }
+        });
+    }
+
+    for (auto &t : threads) {
+        t.join();
+    }
+}
+#endif
+
 void execute()
 {
     // use std::atomic to create atomic objects
@@ -130,42 +130,10 @@ void execute()
         }
 
         std::cout << counter << std::endl;
-    }
-
-#ifdef HAS_ATOMIC_REF
-    // use std::atomic_ref to apply atomic operations to a referenced object
-    {
-        int counter = 0;
-        do_count(counter);
-        std::cout << counter << std::endl;
-    }
-#endif
-
-    // use std::atomic_flag for an atomic Boolean type
-    {
-        std::atomic_flag lock = ATOMIC_FLAG_INIT;  // this macro has been deprecated in C++20
-        int counter = 0;
-
-        std::vector<std::thread> threads;
-        for (int i = 0; i < 10; ++i) {
-            threads.emplace_back([&] () {
-                for (int i = 0; i < 10; ++i) {
-                    while (lock.test_and_set(std::memory_order_acquire));
-                    ++counter;
-                    lock.clear(std::memory_order_release);
-                }
-            });
-        }
-
-        for (auto &t : threads) {
-            t.join();
-        }
-
-        std::cout << counter << std::endl;
         std::cout << std::endl;
     }
 
-    // use atomic type's member functions: load, store and exchange
+    // use member functions: load, store and exchange
     {
         // load and store
         std::thread t1(get_int_value);
@@ -185,36 +153,27 @@ void execute()
         std::cout << std::endl;
     }
 
-    // use atomic type's member functions: fetch_add and fetch_sub
+    // use member functions: fetch_add and fetch_sub
     {
-        std::atomic<int> sum { 0 };
-        std::vector<int> numbers = generate_random();
+        const int thread_count = 10;
+        std::atomic<unsigned long> sum { 0 };
+        std::vector<unsigned int> numbers = generate_random();
 
-        // auto start_expected = std::chrono::high_resolution_clock::now();
-        // auto sum_expected = std::accumulate(std::begin(numbers), std::end(numbers), 0);
-        // auto end_expected = std::chrono::high_resolution_clock::now();
-
-        // auto start = std::chrono::high_resolution_clock::now();
         size_t size = numbers.size();
         std::vector<std::thread> threads;
-        for (int i = 0; i < 10; ++i) {
+        for (int i = 0; i < thread_count; ++i) {
             threads.emplace_back([&sum, &numbers] (size_t const start, size_t const end) {
                 for (size_t i = start; i < end; ++i) {
                     std::atomic_fetch_add_explicit(&sum, numbers[i],
                             std::memory_order_acquire);
                 }
-            }, i * (size / 10), (i + 1) * (size / 10));
+            }, i * (size / thread_count), (i + 1) * (size / thread_count));
         }
 
         for (auto &t : threads) {
             t.join();
         }
-        // auto end = std::chrono::high_resolution_clock::now();
 
-        // std::cout << sum_expected << ": " << (end_expected - start_expected).count() <<
-        //         "ns" << std::endl;
-        // std::cout << sum << ": " << (end - start).count() <<
-        //         "ns" << std::endl;
         std::cout << "sum = " << sum << std::endl;
         std::cout << std::endl;
     }
@@ -240,7 +199,31 @@ void execute()
         std::cout << std::endl;
     }
 
-    // an efficient thread-synchronization mechanism provided in C++20
+    // use std::atomic_flag for an atomic Boolean type
+    {
+        std::atomic_flag lock = ATOMIC_FLAG_INIT;  // deprecated in C++20
+        int counter = 0;
+
+        std::vector<std::thread> threads;
+        for (int i = 0; i < 10; ++i) {
+            threads.emplace_back([&] () {
+                for (int i = 0; i < 10; ++i) {
+                    while (lock.test_and_set(std::memory_order_acquire));
+                    ++counter;
+                    lock.clear(std::memory_order_release);
+                }
+            });
+        }
+
+        for (auto &t : threads) {
+            t.join();
+        }
+
+        std::cout << counter << std::endl;
+        std::cout << std::endl;
+    }
+
+    // efficient thread-synchronization mechanism provided in C++20
     {
         std::atomic<bool> all_done { false };
         std::atomic<unsigned> completed {};
@@ -261,7 +244,17 @@ void execute()
 
         all_done.wait(false);
         std::cout << "all " << completed.load() << " tasks done" << std::endl;
+        std::cout << std::endl;
     }
+
+#ifdef HAS_ATOMIC_REF
+    // use std::atomic_ref to apply atomic operations to a referenced object
+    {
+        int counter = 0;
+        do_count(counter);
+        std::cout << counter << std::endl;
+    }
+#endif
 }
 
 }
