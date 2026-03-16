676b9d77b8ac11429cab80126853291c37564eec
rhd <szyuanhua@163.com>
2026-03-16 21:32:06 +0800

    recipe_03_06: review

--- 03_functions/recipe_03_06.h
+++ 03_functions/recipe_03_06.h
@@ -2,110 +2,98 @@
 
 #include "recipe_03_common.h"
 
-// #define SHOW_OVERLOADED_FUNCTIONS
-
 namespace recipe_03_06
 {
+using namespace std::string_literals;
 
-template <typename... Ts>
-auto add_l(Ts... args)
+template <typename ...Ts>
+auto add(Ts... args)
 {
-#ifdef SHOW_OVERLOADED_FUNCTIONS
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-#endif
     return (... + args);
 }
 
-template <typename... Ts>
-auto add_to_one_l(Ts... args)
+template <typename T, typename ...Ts>
+auto add_to(T init, Ts... args)
 {
-#ifdef SHOW_OVERLOADED_FUNCTIONS
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-#endif
-    return (1 + ... + args);
+    return (init + ... + args);
 }
 
-template <typename... Ts>
-auto add_r(Ts... args)
-{
-#ifdef SHOW_OVERLOADED_FUNCTIONS
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-#endif
-    return (args + ...);
-}
-
-template <typename... Ts>
-auto add_to_one_r(Ts... args)
-{
-#ifdef SHOW_OVERLOADED_FUNCTIONS
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-#endif
-    return (args + ... + 1);
-}
-
-template <typename... Ts>
+template <typename ...Ts>
 auto all_of(Ts... args)
 {
     return (... && args);
 }
 
-template <typename... Ts>
+template <typename ...Ts>
 auto any_of(Ts... args)
 {
     return (... || args);
 }
 
+template <typename ...Ts>
+void print_and_count(Ts... args)
+{
+    int count = 0;
+    ((std::cout << args << " ", ++count), ...);
+    std::cout << "(" << count << ")" << std::endl;
+}
+
 template <typename T>
-struct wrapper_min
+struct wrapper
 {
     T const &value;
 };
 
 template <typename T>
-constexpr auto operator <(wrapper_min<T> const &lhs, wrapper_min<T> const &rhs)
+constexpr auto operator <(wrapper<T> const &lhs, wrapper<T> const &rhs)
 {
-    return wrapper_min<T> {
+    return wrapper<T> {
         (lhs.value < rhs.value) ? lhs.value : rhs.value
     };
 }
 
-template <typename... Ts>
-constexpr auto min(Ts&&... args)
+template <typename ...Ts>
+constexpr auto min(Ts &&...args)
 {
-    return (wrapper_min<Ts> { args } < ...).value;
+    return (wrapper<Ts> { args } < ...).value;
+}
+
+template <typename ...Ts>
+constexpr auto minimum(Ts &&...args)
+{
+    return (args < ...);
 }
 
 void execute()
 {
     // fold a parameter pack over a binary operator
     {
-        std::cout << add_l(1) << std::endl;
-        std::cout << add_l(1, 2, 3, 4, 5) << std::endl;
-        std::cout << add_to_one_l(1, 2, 3, 4, 5) << std::endl;
-        std::cout << add_r(1) << std::endl;
-        std::cout << add_r(1, 2, 3, 4, 5) << std::endl;
-        std::cout << add_to_one_r(1, 2, 3, 4, 5) << std::endl;
-    }
-
-    // an implementation of all_of function template
-    {
-        std::cout << std::boolalpha << all_of(true, true, true) << std::endl;
-        std::cout << all_of(false, false, false) << std::endl;
-        std::cout << all_of(true, false, true) << std::endl;
+        std::cout << add(1) << std::endl;
+        std::cout << add(1, 2, 3, 4, 5) << std::endl;
+        std::cout << add_to(100, 1, 2, 3, 4, 5) << std::endl;
+        std::cout << add("Hello"s) << std::endl;
+        std::cout << add("Hello"s, ","s, " "s, "Linux"s) << std::endl;
+        std::cout << add_to("Hello"s, ',', ' ', 'L', 'i', 'n', 'u', 'x') << std::endl;
         std::cout << std::endl;
     }
 
-    // an implementation of any_of function template
+    // all_of, any_of and print_and_count function templates
     {
-        std::cout << any_of(true, true, true) << std::endl;
-        std::cout << any_of(false, false, false) << std::endl;
-        std::cout << any_of(true, false, true) << std::endl;
+        std::cout << std::boolalpha << all_of(true, true, true) << ", " <<
+                all_of(false, false, false) << ", " <<
+                all_of(true, false, true) << std::endl;
+        std::cout << std::boolalpha << any_of(true, true, true) << ", " <<
+                any_of(false, false, false) << ", " <<
+                any_of(true, false, true) << std::endl;
+        print_and_count(1, 'a', 3.14, "hello");
+
         std::cout << std::endl;
     }
 
-    // fold expressions that work with arbitrary binary functions
+    // fold expression that works with binary function
     {
-        std::cout << min(10, 3, 4, 8, 5) << std::endl;
+        std::cout << min(5, 3, 4) << std::endl;
+        std::cout << std::boolalpha << minimum(5, 3, 4) << std::endl;
     }
 }
 
